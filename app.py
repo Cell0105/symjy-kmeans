8a82992e9bc37494bd6f04db5c2e7188d735f3f5
Cell0105 <marcel01052005@gmail.com>
2025-06-25 22:31:28 +0700

    tambah port di app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def download(filename):
     return send_file(path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 4000))
+   app.run(host="0.0.0.0", port=port)
